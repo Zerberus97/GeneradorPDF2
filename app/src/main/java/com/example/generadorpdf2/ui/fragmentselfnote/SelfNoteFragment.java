3c085f0c1576fb8539ce1c1d8bfb17a20c862def
Cristopher <nachohernanloyo@gmail.com>
2019-03-28 02:22:38 -0300

    Mejoras del formulario

--- app/src/main/java/com/example/generadorpdf2/ui/fragmentselfnote/SelfNoteFragment.java
+++ app/src/main/java/com/example/generadorpdf2/ui/fragmentselfnote/SelfNoteFragment.java
@@ -1,67 +1,56 @@
 package com.example.generadorpdf2.ui.fragmentselfnote;
 
-import android.app.Activity;
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Checkable;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.generadorpdf2.R;
+import com.itextpdf.text.Annotation;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.MarkedObject;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.ResourceBundle;
 
-import javax.xml.transform.Result;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXResult;
-import javax.xml.transform.stream.StreamSource;
-
-
-
 
 
 public class SelfNoteFragment extends Fragment implements View.OnClickListener {
 
     //------------------Declarando Variables-----------------------------------------
     private View mRootView;
-    private EditText mSubjectEditText, mBodyEditText;
-    private Button mSaveButton, mSendButton;
+    private EditText txtNombreEmpresa, txtDireccionEmpresa;
+    private Button btnGuardar, btnEnviar;
+    private RadioGroup RGroup;
+    private RadioButton RadioServicio, RadioControl;
     private File myFile;
     private String NombreEmpresa = "CyberPunk";
     private String Direccion = "Calle Falsa #123";
+
+    private String TipoServicio = "";
 
 
 
@@ -80,25 +69,37 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mRootView = inflater.inflate(R.layout.fragment_self_note, container, false);
-        mSubjectEditText = (EditText) mRootView.findViewById(R.id.edit_text_subject);
-        mBodyEditText = (EditText) mRootView.findViewById(R.id.edit_text_body);
-        mSaveButton = (Button) mRootView.findViewById(R.id.button_save);
-        mSendButton = (Button) mRootView.findViewById(R.id.button_send);
+        txtNombreEmpresa = (EditText) mRootView.findViewById(R.id.EditTextNombreEmpresa);
+        txtDireccionEmpresa = (EditText) mRootView.findViewById(R.id.EditTextDireccionEmpresa);
+        btnGuardar = (Button) mRootView.findViewById(R.id.button_save);
+        btnEnviar = (Button) mRootView.findViewById(R.id.button_send);
+        RGroup = (RadioGroup)mRootView.findViewById(R.id.RGroup);
+        RadioServicio = (RadioButton) mRootView.findViewById(R.id.RServicio);
+        RadioControl = (RadioButton) mRootView.findViewById(R.id.RControl);
 
         //Boton de guardar
-        mSaveButton.setOnClickListener(new View.OnClickListener() {
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mSubjectEditText.getText().toString().isEmpty()){
-                    mSubjectEditText.setError("El Campo esta vacio");
-                    mSubjectEditText.requestFocus();
+                if (txtNombreEmpresa.getText().toString().isEmpty()){
+                    txtNombreEmpresa.setError("El Campo esta vacio");
+                    txtNombreEmpresa.requestFocus();
                     return;
                 }
-                if (mBodyEditText.getText().toString().isEmpty()){
-                    mBodyEditText.setError("El Campo esta vacio");
-                    mBodyEditText.requestFocus();
+                if (txtDireccionEmpresa.getText().toString().isEmpty()){
+                    txtDireccionEmpresa.setError("El Campo esta vacio");
+                    txtDireccionEmpresa.requestFocus();
                     return;
                 }
+
+                    //Comprobacion de los RadioButton
+                if (RadioControl.isChecked()==true){
+                    TipoServicio = "Control";
+                }else
+                    if (RadioServicio.isChecked()==true){
+                        TipoServicio = "Servicio";
+                    }
+
 
                 try {
 
@@ -122,7 +123,7 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
             });
 
         //Boton de Enviar
-        mSendButton.setOnClickListener(new View.OnClickListener() {
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 emailNote();
@@ -155,11 +156,12 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
         PdfWriter pdfW = PdfWriter.getInstance(document, new FileOutputStream(pdfFolder));
         PdfWriter.getInstance(document, output);
 
-        //Abriendo el documento uy cargando datos
+        //Abriendo el documento y cargando datos
         document.open();
         document.addAuthor("AntiMouse");
-        //document.add(new Paragraph(mSubjectEditText.getText().toString()));
-        //document.add(new Paragraph(mBodyEditText.getText().toString()));
+        //document.add(new Paragraph(txtNombreEmpresa.getText().toString()));
+        //document.add(new Paragraph(txtDireccionEmpresa.getText().toString()));
+        document.add(new Paragraph(TipoServicio));
 
         XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
         String htmlToPdf = "<html>" +
@@ -168,9 +170,9 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
                 "<body bgcolor='#E6E6FA'>" +
                 "<p>" + "Este es un PDF de prueba"+"</p>" +
 
-                "<h2> Nombre Empresa: "+ mBodyEditText.getText().toString() + "</h2>" +
+                "<h2> Nombre Empresa: "+ txtDireccionEmpresa.getText().toString() + "</h2>" +
                 "</body> " +
-                "<h1>"+ mSubjectEditText.getText().toString() + "</h1> " +
+                "<h1>"+ txtNombreEmpresa.getText().toString() + "</h1> " +
                 "</html> ";
 
         String XHTML ="";
@@ -201,8 +203,8 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
 
     private void emailNote() {
         Intent email = new Intent(Intent.ACTION_SEND);
-        email.putExtra(Intent.EXTRA_SUBJECT,mSubjectEditText.getText().toString());
-        email.putExtra(Intent.EXTRA_TEXT, mBodyEditText.getText().toString());
+        email.putExtra(Intent.EXTRA_SUBJECT, txtNombreEmpresa.getText().toString());
+        email.putExtra(Intent.EXTRA_TEXT, txtDireccionEmpresa.getText().toString());
         Uri uri = Uri.parse(myFile.getAbsolutePath());
         email.putExtra(Intent.EXTRA_STREAM, uri);
         email.setType("message/rfc822");
@@ -210,8 +212,8 @@ public class SelfNoteFragment extends Fragment implements View.OnClickListener {
     }
 
     private void Limpiar(){
-        mSubjectEditText.setText("");
-        mBodyEditText.setText("");
+        txtNombreEmpresa.setText("");
+        txtDireccionEmpresa.setText("");
     }
 
 
